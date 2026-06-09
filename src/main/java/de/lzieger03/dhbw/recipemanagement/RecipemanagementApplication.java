33de99e9fe8bef57c1b0af17b607533456c95030
lzieger03 <81645844+lzieger03@users.noreply.github.com>
2026-06-09 09:56:47 +0200

    initial project setup via Spring Initializr

---
+++ src/main/java/de/lzieger03/dhbw/recipemanagement/RecipemanagementApplication.java
@@ -0,0 +1,13 @@
+package de.lzieger03.dhbw.recipemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lzieger03/dhbw/recipemanagement/RecipemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lzieger03.dhbw.recipemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
